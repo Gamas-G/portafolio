1af76bce4bc5dcf3478abe04c92a5e268b304ee3
Gerardo Gamas <gamasgerardo@outlook.com>
2024-02-24 03:47:11 -0500

    Se modifica consumo de datos, por medio de firebase, se cambian las rutas de imagenes, svg por ruta de firebase.

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,24 +10,28 @@ export class InfoPaginaService {
   public cv: string = "";
   public proyectos:IProyecto[]= [];
   public habilidades: IHabilidades[] = [];
+  public me: string = "";
     
   constructor( private http: HttpClient ) { 
     this.cargarPortafolio(); 
   }
 
-  cargarPortafolio(): void{
-    // return new Promise( (resolve, reject) => {
-
-    this.http.get('assets/data/data.json')
+  cargarPortafolio(){
+    
+    return new Promise( (resolve, reject) => {
+    // this.http.get('assets/data/data.json')
+    this.http.get('https://my-portafolio-1c347-default-rtdb.firebaseio.com/portafolio.json')
       .subscribe( (resp: any) => {
         console.log(resp);
         this.speetch   = resp.data.speetch;
         this.cv        = resp.data.cv;
         this.proyectos = resp.data.proyectos;
         this.habilidades = resp.data.habilidades;
+
+        this.me = resp.data.me;
       });
 
-    // });
+    });
   }
 
     
